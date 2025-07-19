ae4d1f782c01b5f8221545cc90377529d595ea29
cops9080 <up93py@gmail.com>
2025-07-19 20:12:38 +0900

    Title: 데이터 교환, Time: 87ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250719/데이터 교환/exchange-data.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int c = 7;
+        int temp1;
+        int temp2;
+
+        temp1 = b;
+        temp2 = c;
+
+        b = a;
+        c = temp1;
+        a = temp2;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
