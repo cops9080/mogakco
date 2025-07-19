258575887344eaf49f087a7a1eedeba6210be218
cops9080 <up93py@gmail.com>
2025-07-19 19:57:13 +0900

    Title: 길이 단위 변환하기, Time: 95ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250719/길이 단위 변환하기/change-length-unit.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double f = 30.48;
+        int m = 160934;
+
+        System.out.printf("9.2ft = %.1fcm",(9.2*f));
+        System.out.println();
+        System.out.printf("1.3mi = %.1fcm",(1.3*m));
+    }
+}
