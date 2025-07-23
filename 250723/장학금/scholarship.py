8a22c4d6072ce7ace3143dc511cb767cebe1db7b
cops9080 <up93py@gmail.com>
2025-07-23 12:06:19 +0900

    Title: 장학금, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250723/장학금/scholarship.py
+++ 250723/장학금/scholarship.py
@@ -5,9 +5,9 @@ m, l = map(int, k.split())
 if m>=90:
     if l>=95:
         print("100000")
-    if l>=90:
+    elif l>=90 and l<95:
         print("50000")
     else:
-        print(0)
+        print("0")
 else:
-    print(0)
+    print("0")
