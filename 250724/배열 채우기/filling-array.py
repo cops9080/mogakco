4c968926236e72c47da6c2d7f9c5b08a5d4bb3be
cops9080 <up93py@gmail.com>
2025-07-24 20:22:49 +0900

    Title: 배열 채우기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250724/배열 채우기/filling-array.py
+++ 250724/배열 채우기/filling-array.py
@@ -1,6 +1,12 @@
 arr = list(map(int, input().split()))
 n = len(arr)
 
-for i in range(n-1, -1, -1):
-    if arr[i] != 0:
-        print(arr[i], end=' ')
+for i in range(0, n):
+    if arr[i] == 0:
+        for _ in range(n-i):
+            arr.pop()
+        break
+
+N = len(arr)
+for i in range(N-1, -1, -1):
+    print(arr[i], end=' ')
