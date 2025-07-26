9d432d65247d073a1ae3a912cec6ec18050e9fe5
cops9080 <up93py@gmail.com>
2025-07-26 18:25:39 +0900

    Title: 황금비율 토스트, Time: 142ms, Memory: 10MB, Status: Passed - Codetree

--- 250726/황금비율 토스트/golden-toast.java
+++ 250726/황금비율 토스트/golden-toast.java
@@ -1,16 +1,16 @@
-import java.util.Scanner;
-import java.util.ListIterator;
-import java.util.LinkedList;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
         LinkedList<Character> l = new LinkedList<>();
 
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        String s = sc.next();
-        sc.nextLine();
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        String s = br.readLine();
         
         for (int i=0; i<s.length(); i++) {
             l.add(s.charAt(i));
@@ -19,7 +19,8 @@ public class Main {
         ListIterator<Character> it = l.listIterator(l.size());
 
         for (int i = 0; i < m; i++) {
-            String command = sc.nextLine();
+            String command = br.readLine();
+            
             if (command.equals("L")) {
                 if (it.hasPrevious()) {
                     it.previous();
