e818abb7e747a2deb3d1f162ecc41c45f9d61d01
cops9080 <up93py@gmail.com>
2025-07-29 20:18:17 +0900

    Title: 사는 지역, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250729/사는 지역/where-live.py
+++ 250729/사는 지역/where-live.py
@@ -1,7 +1,7 @@
 n = int(input())    
 
 class Information:
-    def __init__(self, name, street_address, region)
+    def __init__(self, name, street_address, region):
         self.name = name
         self.address = street_address
         self.region = region
@@ -10,6 +10,7 @@ inputs = [input().split() for _ in range(n)]
 
 people = [Information(name, street_address, region) for name, street_address, region in inputs]
 
-people = list(sorted(people, key=lambda x: x.name))
+people = list(sorted(people, key=lambda x: x.name, reverse=True))
 print(f"name {people[0].name}")
-print(f"name {people[0].name}")
+print(f"addr {people[0].address}")
+print(f"city {people[0].region}")
