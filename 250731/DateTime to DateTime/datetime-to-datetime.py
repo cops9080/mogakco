50e8bc0a58394c94b9ee80be8a7102011ffb741e
cops9080 <up93py@gmail.com>
2025-07-31 19:52:56 +0900

    Title: DateTime to DateTime, Time: 1098ms, Memory: 16MB, Status: Time Limit Exceed - Codetree

---
+++ 250731/DateTime to DateTime/datetime-to-datetime.py
@@ -0,0 +1,18 @@
+a, b, c = map(int, input().split())
+D = 11
+H = 11
+M = 11
+
+elapsed_minute = 0
+while True:
+    if D == a and H == b and M == c:
+        break
+    if H >= 24:
+        D += 1
+        H = 0
+    if M >= 60:
+        H += 1
+        M = 0
+    M += 1
+    elapsed_minute += 1
+print(elapsed_minute)
