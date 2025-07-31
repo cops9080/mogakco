551acb23ce930400395a693a044b88866ba3a646
cops9080 <up93py@gmail.com>
2025-07-31 18:53:08 +0900

    Title: 최대로 겹치는 구간, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

--- 250731/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 250731/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -2,14 +2,11 @@ n = int(input())
 segments = [tuple(map(int, input().split())) for _ in range(n)]
 
 line = [0] * 200
+min_point = min(segments)[0]
 
 for x1, x2 in segments:
-    if x1 * x2 < 0 and x1 < x2:
-        x1 = 0
-        x2 = x2 + abs(x1)
-    elif x1 * x2 < 0 and x1 > x2:
-        x2 = 0
-        x1 = x1 + abs(x2)
+    x1 += abs(min_point)
+    x2 += abs(min_point)
     for i in range(x1, x2):
         line[i] += 1
 
