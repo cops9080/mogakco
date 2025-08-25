67e319c38c303440c0e0eb0ec1a33621501ab4be
cops9080 <up93py@gmail.com>
2025-08-26 04:06:42 +0900

    Title: 체크판위에서 2, Time: 135ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250826/체크판위에서 2/on-the-checkboard-2.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int R = sc.nextInt();
+        int C = sc.nextInt();
+        char[][] grid = new char[R][C];
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                grid[i][j] = sc.next().charAt(0);
+            }
+        }
+
+        int idx = 0;
+        int jump = 0;
+        int successCnt = 0;
+        for (int i=0; i<R-1; i++) {
+            for (int k=0; k<C-i; k++) {
+                    char beforeJump = grid[i][k];
+                for (int j=0; j<C-1; j++) {
+                    if (beforeJump != grid[i+1][j+1]) {
+                        jump++;
+                    }
+                }
+                if (jump == 2) {
+                    successCnt += 1;
+                    jump = 0;
+                }
+            }
+        }
+        System.out.print(successCnt);
+    }
+}
