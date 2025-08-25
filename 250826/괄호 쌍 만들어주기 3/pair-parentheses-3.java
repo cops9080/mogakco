a7633d50a8f2979d8071b59574663c7df8adb4c5
cops9080 <up93py@gmail.com>
2025-08-26 02:58:45 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 115ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250826/괄호 쌍 만들어주기 3/pair-parentheses-3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        
+        int cnt = 0;
+        for (int i=1; i<str.length(); i++) {
+            char open = str.charAt(i);
+            if (open == '(') {
+                for (int j=i+1; j<str.length(); j++) {
+                    char close = str.charAt(j);
+                    if (close == ')') {
+                        cnt++;
+                    }
+                }
+            }
+        }
+        System.out.print(cnt);
+    }
+}
