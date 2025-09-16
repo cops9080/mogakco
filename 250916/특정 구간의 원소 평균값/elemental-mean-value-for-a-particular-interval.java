1c781aa2237b5e861a2f218a66885b0c547220a3
이재준 <up93py@gmail.com>
2025-09-16 21:24:14 +0900

    Title: 특정 구간의 원소 평균값, Time: 162ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250916/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        
+        int sumVal = 0;
+        int maxVal = 0;
+        int cnt = 0;
+        int mean = 0;
+
+        for(int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+
+                //부분수열의 합 계산
+                sumVal = 0;
+                for (int k = i; k < j; k++) {
+                    sumVal += arr[k];
+                }
+                
+                int length = j - i;
+                if (i == j) {
+                    length = 1;
+                }
+                
+                if (sumVal % length == 0) {
+                    mean = sumVal / length;
+                }
+                
+                boolean found = false;
+                for (int k = i; k < j; k++) {
+                    if (arr[k] == mean) {
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (found) {
+                    cnt++;
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+}
