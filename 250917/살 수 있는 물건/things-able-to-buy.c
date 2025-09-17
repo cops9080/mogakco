21626e08c3053a265b25345b69aa5e96f286d7c0
이재준 <up93py@gmail.com>
2025-09-17 18:37:07 +0900

    Title: 살 수 있는 물건, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250917/살 수 있는 물건/things-able-to-buy.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if (n >= 3000) {
+        printf("book");
+    } else if (n < 1000) {
+        printf("no");
+    } else {
+        printf("mask");
+    }
+    return 0;
+}
